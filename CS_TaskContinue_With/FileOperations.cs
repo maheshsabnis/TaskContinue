2cb56d097c4f433dc7e838b051ac52b21728e0ff
Mahesh <sabnis_m@hotmail.com>
2025-04-04 11:13:13 +0000

    Add project files.

---
+++ CS_TaskContinue_With/FileOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_TaskContinue_With
+{
+    internal class FileOperations
+    {
+        public string ReadFileOne(string fileName)
+        {
+            try
+            {
+                FileStream Fs = File.OpenRead(fileName);
+                StreamReader sr = new StreamReader(Fs);
+                string fileContent = sr.ReadToEnd();
+                sr.Close();
+                Fs.Close();
+                return fileContent;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool WriteFile(string fileName, string content)
+        {
+            FileStream fs = File.Create(fileName);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(content);
+            sw.Close();
+            fs.Close();
+            return true;
+        }
+    }
+}
---
+++ CS_TaskContinue_With/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace CS_TaskContinue_With
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperations operations = new FileOperations();
+            Console.WriteLine("DEMO Task Continue With");
+            try
+            {
+                Task task = Task.Factory.StartNew<string>(() =>
+                {
+                    try
+                    {
+                        string fileContent = operations.ReadFileOne(@"e:\file11.txt");
+                        Console.WriteLine(fileContent);
+                        return fileContent;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }).ContinueWith(t =>
+                {
+                    try
+                    {
+                        if (t.Status == TaskStatus.Faulted)
+                        {
+                            throw t.Exception;
+                        }
+                        else
+                        {
+                            var result = operations.WriteFile(@"e:\file2.txt", t.Result);
+                            if (result)
+                            {
+                                Console.WriteLine("Data is written in the file");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Write File Failed..");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error Occurred in Task {ex.Message}");
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
